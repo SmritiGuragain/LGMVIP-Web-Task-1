e941fb6dbe74949eec66a0d89bcaa0f26a5fa56d
SmritiGuragain <81467033+SmritiGuragain@users.noreply.github.com>
2024-01-07 08:33:19 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const inputbox = document.getElementById("input-box");
+const container = document.getElementById("list-items");
+
+function addTask() {
+    if (inputbox.value === '') {
+        alert("You must enter task.");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        container.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value="";
+}
+
+container.addEventListener("click", function(e){
+    if (e.target.tagName === "LI")
+    {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+    }
+},false);
